362bd53df1cdf3d4cb4390a9153e067818a1659d
devGusta2 <gustavo.souza2979@gmail.com>
2024-03-20 12:14:11 -0300

    Modificacao

--- Aula05/src/App.java
+++ Aula05/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! 20/03/2024");
     }
 }
